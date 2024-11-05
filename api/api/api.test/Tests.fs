9692d34d1349cfcb00c6248f27c65a1ffc65d87a
Casper Weiss Bang <c@cwb.dk>
2024-11-05 14:50:21 +0100

    refactor

--- api/api/api.test/Tests.fs
+++ api/api/api.test/Tests.fs
@@ -4,7 +4,6 @@ open System.Net
 open System.Net.Http
 open System.Net.Http.Json
 open System.Threading.Tasks
-open Microsoft.AspNetCore.Http
 open Xunit
 open FsCheck.FSharp
 open FsCheck.Xunit
@@ -12,47 +11,48 @@ open api.test
 open api.Controllers
 
 module Assert =
+    let equal (a: 'T) (b: 'T) = Assert.Equal<'T>(a, b)
+
     let hasStatusCode (expected: HttpStatusCode) (response: HttpResponseMessage Task) =
         task {
-            let! response = response 
-            Assert.Equal(expected, response.StatusCode)
+            let! response = response
+            equal expected response.StatusCode
         }
-        
+
 module Http =
-   let get (path: string) (client: HttpClient) = 
-        client.GetAsync(path)
+    let get (path: string) (client: HttpClient) = client.GetAsync(path)
+    let getJson<'T> (path: string) (client: HttpClient) = client.GetFromJsonAsync<'T>(path)
 
 [<Property>]
 let ``Can get swagger`` =
-    APIClient.getClientWithUsers >> Http.get "/Swagger" >> Assert.hasStatusCode HttpStatusCode.OK
-    
+    APIClient.getClientWithUsers
+    >> Http.get "/Swagger"
+    >> Assert.hasStatusCode HttpStatusCode.OK
+
 [<Property>]
 let ``GetAll returns all entries`` users =
     task {
         let client = APIClient.getClientWithUsers users
-        
-        let! result  = client.GetFromJsonAsync<List<UserDto>>("/User/")
-    
-        let expected = users |> List.map UserDto.fromDomain 
+
+        let! result = client.GetFromJsonAsync<List<UserDto>>("/User/")
+
+        let expected = users |> List.map UserDto.fromDomain
         Assert.Equivalent(expected, result)
     }
+
 [<Property>]
 let ``GetUser returns user if in list`` users user =
-    task {
-        let users = user :: users
-        let client = APIClient.getClientWithUsers users
-        
-        let! result  = client.GetFromJsonAsync<UserDto>($"/User/{user.id.ToString()}/")
+    let client = APIClient.getClientWithUsers (user :: users)
     
-        let expected = UserDto.fromDomain user
-        Assert.Equal(expected, result)
-    }
+    client
+    |> Http.getJson<UserDto> $"/User/{user.id.ToString()}/"
+    |> Task.map (Assert.equal (UserDto.fromDomain user))
+
 [<Property>]
 let ``GetUser returns 404 if not in list`` users user =
-    not (List.contains user users) ==> task {
-        let client = APIClient.getClientWithUsers users
-        
-        let! response  = client.GetAsync($"/User/{user.id.ToString()}/")
-    
-        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode)
-    }
+    not (List.contains user users)
+    ==> (let client = APIClient.getClientWithUsers users
+
+         client
+         |> Http.get $"/User/{user.id.ToString()}/"
+         |> Assert.hasStatusCode HttpStatusCode.NotFound)
