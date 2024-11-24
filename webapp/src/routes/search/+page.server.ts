c8c3f6f3eebe99df0d7a4f41586f6f642d303202
Casper Weiss Bang <c@cwb.dk>
2024-11-24 20:41:28 +0100

    remove debug

--- webapp/src/routes/search/+page.server.ts
+++ webapp/src/routes/search/+page.server.ts
@@ -3,7 +3,6 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = ({ url }) => {
 	const query = url.searchParams.get('q') || '';
-	console.log('Query is: ' + query);
 	const service = new PlantService();
 
 	return { plants: service.search(query), query };
