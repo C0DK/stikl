4dc96a05631f1fe4206b6af9a0c311a2d5dcccd2
Casper Weiss Bang <c@cwb.dk>
2025-06-29 11:15:22 +0200

    chore: remove webapp!

--- webapp/src/routes/plant/[id]/Tag.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	let { label }: { label: string } = $props();
-</script>
-
-<span
-	class="dark:text-white-300 me-2 rounded bg-amber-600 px-2.5 py-0.5 text-xs font-semibold text-white dark:bg-amber-900"
-	>{label}</span
->
--- webapp/src/routes/search/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { PlantService } from '$lib/services/plant';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ url }) => {
-	const query = url.searchParams.get('q') || '';
-	const service = new PlantService();
-
-	return { plants: await service.search(query), query };
-};
