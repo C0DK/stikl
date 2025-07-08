dd9f1d4939311ed7da89dd216ddeb807ad71ae4a
Casper Weiss Bang <c@cwb.dk>
2025-07-08 23:26:17 +0200

    chore: setup SSL

--- infra/main.nix
+++ infra/main.nix
@@ -1,55 +1,82 @@
 { config, pkgs, ... }:
 {
-  config.environment.systemPackages = with pkgs; [
-    git
-    vim
-    # https://taskfile.dev/
-    go-task
+  config = {
+    environment.systemPackages = with pkgs; [
+      git
+      vim
+      # https://taskfile.dev/
+      go-task
 
-  ];
-
-  config.services.postgresql = {
-    enable = true;
-    ensureDatabases = [ "stikl" ];
-    enableTCPIP = true;
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database DBuser origin-address auth-method
-      local all       all     trust
-      # ipv4
-      host  all      all     127.0.0.1/32   trust
-      host  all      all     ::1/128   trust
-    '';
-  };
-
-  # https://bkiran.com/blog/deploying-containers-nixos
-  config.virtualisation = {
-    podman = {
+    ];
+    services.nginx = {
       enable = true;
-    };
 
-    oci-containers = {
-      #backend= "docker";
-      containers = {
-        stikl-web = {
-          # todo login if private?
-          login = {
-            registry = "https://ghcr.io";
-            username = "C0DK";
-            passwordFile = "/etc/stikl/registry-password.txt";
-          };
-          image = "ghcr.io/c0dk/stikl:main";
-          environment = {
-            DEV_MODE = "false";
-          };
-          environmentFiles = [
-            ../app/.env
-          ];
-          ports = [ "80:8080" ];
-          pull = "always";
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
 
+      virtualHosts."stikl.dk" = {
+        enableACME = true;
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8080";
+          proxyWebsockets = true; # needed if you need to use WebSocket
+          extraConfig =
+            # required when the target is also TLS server with multiple hosts
+            "proxy_ssl_server_name on;"
+            +
+              # required when the server wants to use HTTP Authentication
+              "proxy_pass_header Authorization;";
         };
       };
     };
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "c@cwb.dk";
+    };
 
+    services.postgresql = {
+      enable = true;
+      ensureDatabases = [ "stikl" ];
+      enableTCPIP = true;
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type database DBuser origin-address auth-method
+        local all       all     trust
+        # ipv4
+        host  all      all     127.0.0.1/32   trust
+        host  all      all     ::1/128   trust
+      '';
+    };
+
+    # https://bkiran.com/blog/deploying-containers-nixos
+    virtualisation = {
+      podman = {
+        enable = true;
+      };
+
+      oci-containers = {
+        #backend= "docker";
+        containers = {
+          stikl-web = {
+            # todo login if private?
+            login = {
+              registry = "https://ghcr.io";
+              username = "C0DK";
+              passwordFile = "/etc/stikl/registry-password.txt";
+            };
+            image = "ghcr.io/c0dk/stikl:main";
+            environment = {
+              DEV_MODE = "false";
+            };
+            environmentFiles = [
+              ../app/.env
+            ];
+            ports = [ "8080:8080" ];
+            pull = "always";
+
+          };
+        };
+      };
+
+    };
   };
 }
