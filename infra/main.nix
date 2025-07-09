770c4a39fc7db3b62de63344e11373409e3b8286
Casper Weiss Bang <c@cwb.dk>
2025-07-10 01:29:20 +0200

    nginx ffs

--- infra/main.nix
+++ infra/main.nix
@@ -45,6 +45,10 @@
             proxy_headers_hash_max_size 1024;
             # Increase the bucket size of the hash table
             proxy_headers_hash_bucket_size 128;
+
+            proxy_busy_buffers_size   512k;
+            proxy_buffers   4 512k;
+            proxy_buffer_size   256k;
           '';
         };
       };
