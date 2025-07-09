8f2a2d082265a78f26f4dd31122bbe0737683f3b
Casper Weiss Bang <c@cwb.dk>
2025-07-10 01:10:37 +0200

    chore: change code to see if deployed

--- app/Stikl.Web/Pages/Index.fs
+++ app/Stikl.Web/Pages/Index.fs
@@ -12,7 +12,7 @@ let render (pageBuilder: PageBuilder) =
     let callToAction =
         """
         <p class="mb-8 max-w-md text-center text-lg md:text-xl">
-            Deltag i ET fælleskab hvor vi gratis deler frø, planer og stiklinger. At undgå industrielt voksede planter er ikke bare billigere for dig - men også for miljøet.
+            Find gratis frø, planer og stiklinger i dit nærområde, og kom af med dine overskydende stiklinger. At undgå industrielt voksede planter er ikke bare billigere for dig - men også for miljøet.
         </p>
         """
 
