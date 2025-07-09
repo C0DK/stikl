f0be9b3f814eaebc2a7f8caac00d132e5c618fe2
Casper Weiss Bang <c@cwb.dk>
2025-07-09 23:22:05 +0200

    chore: allow postgres from containers

--- infra/app.nix
+++ infra/app.nix
@@ -18,6 +18,7 @@
         local all       all     trust
         # ipv4
         host  all      all     127.0.0.1/32   trust
+        host  all      all     10.88.0.1/32   trust
         host  all      all     ::1/128   trust
       '';
     };
