55176c1d635d8710f5cae1a369bf5691ad6d5017
Casper Weiss Bang <c@cwb.dk>
2025-07-10 00:30:06 +0200

    nginx ffs

--- infra/main.nix
+++ infra/main.nix
@@ -32,6 +32,7 @@
           proxyPass = "http://10.88.0.1:8080";
           extraConfig = ''
             proxy_set_header   Host $host;
+            proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header   X-Forwarded-Proto $scheme;
             proxy_pass_header  Authorization;
 
