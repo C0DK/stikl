fca0ad779ab59a1d49cd7e1246d535e6f6c1ed49
Casper Weiss Bang <c@cwb.dk>
2025-07-10 00:04:35 +0200

    nginx ffs

--- infra/main.nix
+++ infra/main.nix
@@ -28,7 +28,7 @@
         enableACME = true;
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://127.0.0.1:8080";
+          proxyPass = "http://localhost:8080";
           proxyWebsockets = true; # needed if you need to use WebSocket
           extraConfig = ''
             proxy_set_header   Host $host;
