884cf881d351f9debf2ff7fa3a888a095a88e3f5
Casper Weiss Bang <c@cwb.dk>
2025-07-09 23:33:52 +0200

    chore: nginx podman port

--- infra/main.nix
+++ infra/main.nix
@@ -28,7 +28,7 @@
         enableACME = true;
         forceSSL = true;
         locations."/" = {
-          proxyPass = "http://127.0.0.1:8080";
+          proxyPass = "10.88.0.1:8080";
           proxyWebsockets = true; # needed if you need to use WebSocket
           extraConfig =
             "proxy_http_version 1.1;"
