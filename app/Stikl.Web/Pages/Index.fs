80a7b3a0360277e049487a0458b390ebb7aadc36
Casper Weiss Bang <c@cwb.dk>
2025-07-10 16:41:57 +0200

    chore: fix typo

--- app/Stikl.Web/Pages/Index.fs
+++ app/Stikl.Web/Pages/Index.fs
@@ -12,7 +12,7 @@ let render (pageBuilder: PageBuilder) =
     let callToAction =
         """
         <p class="mb-8 max-w-md text-center text-lg md:text-xl">
-            Find gratis frø, planer og stiklinger i dit nærområde, og kom af med dine overskydende stiklinger. At undgå industrielt voksede planter er ikke bare billigere for dig - men også for miljøet.
+            Find gratis frø, planter og stiklinger i dit nærområde, og kom af med dine overskydende stiklinger. At undgå industrielt voksede planter er ikke bare billigere for dig - men også for miljøet.
         </p>
         """
 
