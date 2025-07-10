8249c473b7e00447bcf7cd422220c679febcbf13
Casper Weiss Bang <c@cwb.dk>
2025-07-10 20:53:29 +0200

    feat: install grafana

--- infra/main.nix
+++ infra/main.nix
@@ -25,34 +25,58 @@
       ];
     };
 
-    services.nginx = {
-      enable = true;
-      recommendedProxySettings = true;
-      recommendedTlsSettings = true;
-      logError = "stderr debug";
+    services = {
+      nginx = {
+        enable = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
+        logError = "stderr debug";
 
-      virtualHosts."stikl.dk" = {
-        enableACME = true;
-        forceSSL = true;
-        locations."/" = {
-          proxyPass = "http://10.88.0.1:8080";
-          extraConfig = ''
-            proxy_pass_header  Authorization;
-            # Increase the maximum size of the hash table
-            proxy_headers_hash_max_size 1024;
-            # Increase the bucket size of the hash table
-            proxy_headers_hash_bucket_size 128;
+        virtualHosts."stikl.dk" = {
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = "http://10.88.0.1:8080";
+            extraConfig = ''
+              proxy_pass_header  Authorization;
+              # Increase the maximum size of the hash table
+              proxy_headers_hash_max_size 1024;
+              # Increase the bucket size of the hash table
+              proxy_headers_hash_bucket_size 128;
 
-            proxy_busy_buffers_size   512k;
-            proxy_buffers   4 512k;
-            proxy_buffer_size   256k;
-          '';
+              proxy_busy_buffers_size   512k;
+              proxy_buffers   4 512k;
+              proxy_buffer_size   256k;
+            '';
+          };
+        };
+        virtualHosts."your.domain" = {
+          addSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://${toString config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}";
+            proxyWebsockets = true;
+            recommendedProxySettings = true;
+          };
         };
       };
-    };
-    security.acme = {
-      acceptTerms = true;
-      defaults.email = "c@cwb.dk";
+      security.acme = {
+        acceptTerms = true;
+        defaults.email = "c@cwb.dk";
+      };
+      grafana = {
+        enable = true;
+        settings = {
+          server = {
+            # Listening Address
+            http_addr = "127.0.0.1";
+            # and Port
+            http_port = 3000;
+            # Grafana needs to know on which domain and URL it's running
+            domain = "grafana.stikl.dk";
+          };
+        };
+      };
     };
   };
 }
