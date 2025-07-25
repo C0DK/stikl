822c249d0990bbda560087cd8a66095a1bea19d3
Casper Weiss Bang <c@cwb.dk>
2025-07-25 12:32:52 +0200

    chore: show timstamp on history

--- app/Stikl.Web/Pages/Auth/Profile.fs
+++ app/Stikl.Web/Pages/Auth/Profile.fs
@@ -18,14 +18,13 @@ type Form =
 
 let relativeTime (t: DateTimeOffset) =
     let delta = DateTimeOffset.UtcNow.Subtract(t)
-    let round v -> Math.round(v)
+    let round (v: float) = Math.Round(v,0 ) |> int
     match delta with
     // Todo localize
     | d when d.TotalMinutes < 2 -> "a few seconds ago"
     | d when d.TotalMinutes < 61 -> $"{round d.TotalMinutes} minutes ago"
-    | d when d.TotalHours < 25 -> $"{round d.TotalHours} minutes ago"
+    | d when d.TotalHours < 48 -> $"{round d.TotalHours} hours ago"
     // TODO: make this more correct
-    | d when d.TotalDays < 2 -> "Yesterday"
     | d when d.TotalDays < 7 -> $"{round d.TotalDays} days ago"
     // TODO correct timezone
     | _ -> t.ToString("s")
@@ -37,8 +36,7 @@ let historySection (user: User) (locale: Localization) =
         user.history
         // TODO table?
         |> Seq.map (fun e ->
-            let ts = e.timestamp.ToString("S") 
-            $"<li>{ts}: {locale.describeEvent e.payload}</li>"
+            $"<li>{relativeTime e.timestamp}: {locale.describeEvent e.payload}</li>"
             )
         |> String.concat "\n"
 
