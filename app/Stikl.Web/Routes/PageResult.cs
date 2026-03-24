294a95de30c47a13daf13f3139715311e51570c9
Casper Weiss Bang <c@cwb.dk>
2026-03-24 19:34:40 +0100

    wip: fix transitions by swapping complete main

--- app/Stikl.Web/Routes/PageResult.cs
+++ app/Stikl.Web/Routes/PageResult.cs
@@ -53,9 +53,9 @@ public class PageResult(string content, string? title = null) : IResult
         {
             // TODO: check if auth has changed, and if yes, also update that!
             response.Headers["HX-Retarget"] = "main";
-            response.Headers["HX-Reswap"] = "innerHTML swap:300ms";
+            response.Headers["HX-Reswap"] = "outerHTML swap:300ms";
             await response.WriteAsync($"<title>{pageTitle}</title>");
-            await response.WriteAsync(content + string.Join("", toasts));
+            await response.WriteAsync("<main>"+content+"</main>" + string.Join("", toasts));
         }
     }
 }
